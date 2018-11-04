942fedb9293900970d1d1cabad01758c9b297a53
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-04 10:06:11 +0100

    Install redux form. Add Field component to the NewPosts.js and hook the NewPosts.js to redux store.

--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react'
-
+import { Field, reduxForm } from 'redux-form'
+// Field is a react componed wired into redux-form
 class NewPosts extends Component {
+  renderTitleField (field) {
+    return (
+      <div className='form-group'>
+        <label>Title</label>
+        <input
+          className='form-control'
+          type='text'
+          {...field.input}
+        />
+      </div>
+
+    )
+  }
+  renderTagsField () {
+
+  }
+
   render () {
     return (
-      <div>
-        PostsNew
-      </div>
+      <form>
+        <Field
+          name='title'
+          component={this.renderTitleField}
+        />
+        <Field
+          name='title'
+          component={this.renderTagsField}
+        />
+      </form>
+
     )
   }
 }
 
-export default NewPosts
+export default reduxForm({
+  // make sure that string attached to form is unique
+  form: 'PostsNewForm'
+})(NewPosts)
