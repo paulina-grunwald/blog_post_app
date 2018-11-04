a46b92a52e66ffe51cd3a7c66949b97beca58ecf
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-11-04 10:39:36 +0100

    Add 3 field form component, define validation function and pass it to reduxForm.

--- src/components/NewPosts.js
+++ src/components/NewPosts.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
 // Field is a react componed wired into redux-form
 class NewPosts extends Component {
-  renderTitleField (field) {
+  renderField (field) {
     return (
       <div className='form-group'>
-        <label>Title</label>
+        <label>{field.label}</label>
         <input
           className='form-control'
           type='text'
@@ -15,20 +15,24 @@ class NewPosts extends Component {
 
     )
   }
-  renderTagsField () {
-
-  }
 
   render () {
     return (
       <form>
         <Field
+          label='Title'
           name='title'
-          component={this.renderTitleField}
+          component={this.renderField}
         />
         <Field
-          name='title'
-          component={this.renderTagsField}
+          label='Tags'
+          name='tags'
+          component={this.renderField}
+        />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
         />
       </form>
 
@@ -36,7 +40,27 @@ class NewPosts extends Component {
   }
 }
 
+function validate (values) {
+  const errors = {}
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = 'Enter a title!'
+  }
+  if (!values.tags) {
+    errors.title = 'Enter a tags!'
+  }
+  if (!values.content) {
+    errors.title = 'Enter post content'
+  }
+
+  // If errors is an empty object, the form is ready to submit
+  // If errors has any properties, redux assumes form is invalid
+  return errors
+}
+
 export default reduxForm({
+  validate,
   // make sure that string attached to form is unique
   form: 'PostsNewForm'
 })(NewPosts)
